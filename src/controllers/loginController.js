a596f630b343a03770979732d9472890c40f00fa
EkaitzGA <ekaitzguerra@gmail.com>
2024-11-13 16:42:57 +0100

    formulario de login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,17 @@
 function login(req,res){
-    res.send("Inicia sesión aquí")
+    res.render("login")
 }
-
+function loginAccess(req,res){
+    const {username,password} = req.body;
+    res.json(req.body) /*Esto es temporal hasta tener acceso */
+   /* res.redirect("/client-profile"); */
+}
+function registerAccess(req,res){
+    res.redirect("/register")
+}
 export const functions ={
-    login
+    login,
+    loginAccess,
+    registerAccess
 }
 export default functions
